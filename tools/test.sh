747dac1c804fb5d658229612d5830f7637d19b91
elian <d2099080@gmail.com>
2024-11-28 21:21:06 +0800

    Refine the generation of netdev

--- tools/test.sh
+++ tools/test.sh
@@ -14,6 +14,7 @@ function check_kmod() {
 
 function insert_kmod() {
     local mod_name=$1
+    local param=$2
     local noko_name=$(echo $mod_name |sed s/.ko//)
     check_kmod $noko_name
     ret=$?
@@ -21,7 +22,7 @@ function insert_kmod() {
         sudo rmmod $noko_name > /dev/null
     fi
     echo "Installing Module $mod_name"
-    sudo insmod $mod_name
+    sudo insmod $mod_name $param
     return $(check_kmod $noko_name)
 } 
 
@@ -37,7 +38,7 @@ function remove_kmod() {
     return 0
 }
 
-insert_kmod viper.ko
+insert_kmod viper.ko num_port=6
 if [ $? -ne 0 ]; then
     return_val=1
 fi
@@ -45,22 +46,22 @@ fi
 if [ $return_val -eq 0 ]; then
     # avoid device or resource busy error
     sleep 0.5
-
+    sudo ip link
     # create network namespaces for each port
     sudo ip netns add ns0
     sudo ip netns add ns1
 
-    sudo ip link set veth0 netns ns0
-    sudo ip link set veth1 netns ns1
-    sudo ip netns exec ns0 ip link set veth0 up
-    sudo ip netns exec ns1 ip link set veth1 up
+    sudo ip link set viper0 netns ns0
+    sudo ip link set viper1 netns ns1
+    sudo ip netns exec ns0 ip link set viper0 up
+    sudo ip netns exec ns1 ip link set viper1 up
 
-    sudo ip netns exec ns0 ip addr add 10.0.0.1/24 dev veth0
-    sudo ip netns exec ns1 ip addr add 10.0.0.2/24 dev veth1
+    sudo ip netns exec ns0 ip addr add 10.0.0.1/24 dev viper0
+    sudo ip netns exec ns1 ip addr add 10.0.0.2/24 dev viper1
 
     echo
     echo "================================================================================"
-    echo "Ping Test: Port0 veth0 (10.0.0.1) <--> Port1 veth1 (10.0.0.2)"
+    echo "Ping Test: Port0 viper0 (10.0.0.1) <--> Port1 viper1 (10.0.0.2)"
     echo
     echo "(should succeed)"
     echo "(be patient, it will take some time to route...)"
--- viper.c
+++ viper.c
@@ -7,8 +7,39 @@
 #include <linux/netdevice.h>
 #include <linux/version.h>
 
-static struct net_device *veth0, *veth1;
-static struct net_device_stats veth_stats0, veth_stats1;
+#define PORT_NAME "viper%d"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Elian Chen");
+MODULE_DESCRIPTION("Virtual Ethernet Switch Driver");
+
+/* Customize the number of port in this switch */
+static int num_port = 4;
+module_param(num_port, int, 0444);
+MODULE_PARM_DESC(num_port, "Number of port in this virtual switch.");
+
+
+/* Global structure to maintain all interface */
+struct viper_content{
+    /* Point to the list which contain all port interface */
+    struct list_head port_list;
+};
+/* Port interface pointed to by netdev_priv() */
+struct port_if{
+    /* Point to the net device */
+    struct net_device *ndev;
+    struct net_device_stats stats;
+    /* Link from viper_content */
+    struct list_head port_list;
+};
+static struct viper_content *viper = NULL; 
+
+/* Retrieve port interface from net device */
+static inline struct port_if* ndev_get_viper_pif(struct net_device *ndev)
+{
+    /* Port interface pointed to by netdev_priv() */
+    return (struct port_if *) netdev_priv(ndev);
+}
 
 // Forwarding Database (FDB) to simulate MAC address learning
 #define FDB_SIZE 256
@@ -45,7 +76,7 @@ static void fdb_add(const unsigned char *mac,
 }
 
 // The packet reception function (switching logic with VLAN support)
-static int veth_rx(struct sk_buff *skb, struct net_device *dev)
+static int viper_rx(struct sk_buff *skb, struct net_device *dev)
 {
     unsigned char *mac_dst = eth_hdr(skb)->h_dest;
     struct net_device *out_dev = NULL;
@@ -63,8 +94,7 @@ static int veth_rx(struct sk_buff *skb, struct net_device *dev)
     }
 
     /*if (!out_dev) {
-        printk(KERN_WARNING "viper: MAC address not found in FDB for VLAN %u,
-    dropping packet.\n", vlan_id); dev_kfree_skb(skb); return NET_RX_DROP;
+        pr_warn("viper: MAC address not found in FDB for VLAN %u, dropping packet.\n", vlan_id); dev_kfree_skb(skb); return NET_RX_DROP;
     }*/
 
     // Forward the packet to the correct device
@@ -80,11 +110,11 @@ static int veth_rx(struct sk_buff *skb, struct net_device *dev)
 }
 
 // The packet transmission function (transmitting logic with VLAN support)
-static netdev_tx_t veth_start_xmit(struct sk_buff *skb, struct net_device *dev)
+static netdev_tx_t viper_start_xmit(struct sk_buff *skb, struct net_device *dev)
 {
     unsigned char *mac_src = eth_hdr(skb)->h_source;
     u16 vlan_id = 0;
-
+    pr_info("viper: src| %pM\n", mac_src);
     // Check if the packet has a VLAN tag and extract the VLAN ID
     if (skb_vlan_tag_present(skb)) {
         vlan_id = skb_vlan_tag_get(skb);
@@ -92,14 +122,8 @@ static netdev_tx_t veth_start_xmit(struct sk_buff *skb, struct net_device *dev)
 
     // Learn the source MAC address and VLAN ID
     fdb_add(mac_src, dev, vlan_id);
-
-    // Simply forward the packet to the other device (veth0 or veth1)
-    if (dev == veth0) {
-        skb->dev = veth1;
-    } else {
-        skb->dev = veth0;
-    }
-    veth_rx(skb, skb->dev);
+    
+    //viper_rx(skb, skb->dev);
     return 0;
     // return dev_queue_xmit(skb);
 }
@@ -107,7 +131,7 @@ static netdev_tx_t veth_start_xmit(struct sk_buff *skb, struct net_device *dev)
 /* Open NIC */
 static int viper_eth_open(struct net_device *netdev)
 {
-    printk(KERN_INFO "viper: %s Ethernet device opened\n", netdev->name);
+    pr_info("viper: %s Ethernet device opened\n", netdev->name);
     netif_start_queue(netdev);
     return 0;
 }
@@ -115,66 +139,97 @@ static int viper_eth_open(struct net_device *netdev)
 /* Close NIC */
 static int viper_eth_stop(struct net_device *netdev)
 {
-    printk(KERN_INFO "viper: %s Ethernet device stopped\n", netdev->name);
+    pr_info("viper: %s Ethernet device stopped\n", netdev->name);
     netif_stop_queue(netdev);
     return 0;
 }
 
 
-// Network device operations
-static const struct net_device_ops veth_netdev_ops = {
-    .ndo_start_xmit = veth_start_xmit,
+/* Net device operations */
+static const struct net_device_ops viper_netdev_ops = {
+    .ndo_start_xmit = viper_start_xmit,
     .ndo_open = viper_eth_open,
     .ndo_stop = viper_eth_stop,
-    .ndo_get_stats =
-        NULL,  // For simplicity, not implementing stats gathering here
+    /* For simplicity, not implementing stats gathering here */
+    .ndo_get_stats = NULL,  
 };
 
-// Initialize virtual Ethernet devices with VLAN support
-static int __init veth_switch_init(void)
-{
-    veth0 = alloc_netdev(0, "veth0", NET_NAME_UNKNOWN, ether_setup);
-    veth1 = alloc_netdev(0, "veth1", NET_NAME_UNKNOWN, ether_setup);
-
-    if (!veth0 || !veth1) {
-        printk(KERN_ERR "viper: Failed to allocate network devices\n");
+static int viper_add_port(int idx){
+    struct net_device *ndev = NULL;
+    /* Allocate net device context */
+    ndev = alloc_netdev(sizeof(struct port_if), PORT_NAME, NET_NAME_ENUM,
+                        ether_setup);
+    if (!ndev){
+        pr_err("viper: Couldn't allocate space for nedv");
         return -ENOMEM;
     }
+    /* Assign viper operations */
+    ndev->netdev_ops = &viper_netdev_ops;
 
-    // Set up the network devices
-    veth0->netdev_ops = &veth_netdev_ops;
-    veth1->netdev_ops = &veth_netdev_ops;
+    /* Asign MAC address.
+     * The first byte is '0x02' to avoid being a multicast
+     * address.
+     */
+    char mac[ETH_ALEN];
+    /* The first 3~24 bits are vendor identifier */
+    mac[0] = 0x02; /* Locally administered address, unicast (binary: 00000010) */
+    mac[1] = 0xab; 
+    mac[2] = 0xcd;
+    /* Assign uniqueness MAC address by idx */
+    mac[3] = idx & (0xFF << 16); 
+    mac[4] = idx & (0xFF << 8);
+    mac[5] = idx & 0xFF;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
+    eth_hw_addr_set(ndev, mac);
+#else
+    memcpy(ndev->dev_addr, mac, ETH_ALEN);
+#endif
+    
 
-    // Register devices with the kernel
-    if (register_netdev(veth0)) {
-        printk(KERN_ERR "viper: Failed to register veth0\n");
+    /* Register net device with the kernel */
+    if (register_netdev(ndev)) {
+        pr_err("viper: Failed to register net device\n");
         return -ENODEV;
     }
-    if (register_netdev(veth1)) {
-        printk(KERN_ERR "viper: Failed to register veth1\n");
-        unregister_netdev(veth0);
-        return -ENODEV;
-    }
 
-    printk(KERN_INFO
-           "viper: Virtual Ethernet switch driver with VLAN support loaded\n");
+    /* Fulfill the port interface */
+    struct port_if *pif = ndev_get_viper_pif(ndev);
+    pif->ndev = ndev;
+    list_add_tail(&pif->port_list, &viper->port_list);
     return 0;
 }
 
-// Cleanup the virtual devices
-static void __exit veth_switch_exit(void)
+/* Initialize viper */
+static int __init viper_switch_init(void)
 {
-    unregister_netdev(veth0);
-    unregister_netdev(veth1);
-    free_netdev(veth0);
-    free_netdev(veth1);
+    /* Initialize global content */
+    viper = kmalloc(sizeof(struct viper_content), GFP_KERNEL);
+    if (!viper) {
+        pr_err("viper: Couldn't allocate space for viper_content\n");
+        return -ENOMEM;
+    }
+    INIT_LIST_HEAD(&viper->port_list);
+    for (int i=0;i<num_port;++i){
+        viper_add_port(i);
+    }
 
-    printk(KERN_INFO "viper: Virtual Ethernet switch driver unloaded\n");
+    pr_info("viper: Virtual Ethernet switch driver loaded\n");
+    return 0;
 }
 
-module_init(veth_switch_init);
-module_exit(veth_switch_exit);
+/* Exit viper */
+static void __exit viper_switch_exit(void)
+{
+    struct port_if *pif=NULL, *safe=NULL;
+    list_for_each_entry_safe(pif, safe, &viper->port_list, port_list){
+        unregister_netdev(pif->ndev);
+        free_netdev(pif->ndev);
+    }
+    kfree(viper);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Elian");
-MODULE_DESCRIPTION("Virtual Ethernet Switch Driver");
+    pr_info("viper: Virtual Ethernet switch driver unloaded\n");
+}
+
+module_init(viper_switch_init);
+module_exit(viper_switch_exit);
+
