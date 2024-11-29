82bb1e7dd86c0fb6d2fb5f29bb976630cc4a2b39
elian <d2099080@gmail.com>
2024-11-29 16:06:15 +0800

    Implement PC/Port interface generation

--- tools/test.sh
+++ tools/test.sh
@@ -14,7 +14,8 @@ function check_kmod() {
 
 function insert_kmod() {
     local mod_name=$1
-    local param=$2
+    local num_port=$2
+    local num_pc=$3
     local noko_name=$(echo $mod_name |sed s/.ko//)
     check_kmod $noko_name
     ret=$?
@@ -22,7 +23,7 @@ function insert_kmod() {
         sudo rmmod $noko_name > /dev/null
     fi
     echo "Installing Module $mod_name"
-    sudo insmod $mod_name $param
+    sudo insmod $mod_name $num_port $num_pc
     return $(check_kmod $noko_name)
 } 
 
@@ -38,7 +39,7 @@ function remove_kmod() {
     return 0
 }
 
-insert_kmod viper.ko num_port=6
+insert_kmod viper.ko num_port=2 num_pc=2
 if [ $? -ne 0 ]; then
     return_val=1
 fi
@@ -48,16 +49,16 @@ if [ $return_val -eq 0 ]; then
     sleep 0.5
     sudo ip link
     # create network namespaces for each port
-    sudo ip netns add ns0
-    sudo ip netns add ns1
+    #sudo ip netns add ns0
+    #sudo ip netns add ns1
 
-    sudo ip link set viper0 netns ns0
-    sudo ip link set viper1 netns ns1
-    sudo ip netns exec ns0 ip link set viper0 up
-    sudo ip netns exec ns1 ip link set viper1 up
+    #sudo ip link set viper0 netns ns0
+    #sudo ip link set viper1 netns ns1
+    #sudo ip netns exec ns0 ip link set viper0 up
+    #sudo ip netns exec ns1 ip link set viper1 up
 
-    sudo ip netns exec ns0 ip addr add 10.0.0.1/24 dev viper0
-    sudo ip netns exec ns1 ip addr add 10.0.0.2/24 dev viper1
+    #sudo ip netns exec ns0 ip addr add 10.0.0.1/24 dev viper0
+    #sudo ip netns exec ns1 ip addr add 10.0.0.2/24 dev viper1
 
     echo
     echo "================================================================================"
@@ -66,12 +67,12 @@ if [ $return_val -eq 0 ]; then
     echo "(should succeed)"
     echo "(be patient, it will take some time to route...)"
     echo "================================================================================"
-    sudo ip netns exec ns0 ping -c 1 10.0.0.2
+    #sudo ip netns exec ns0 ping -c 1 10.0.0.2
 fi
 
 remove_kmod viper
-sudo ip netns del ns0
-sudo ip netns del ns1
+#sudo ip netns del ns0
+#sudo ip netns del ns1
 
 if [ $return_val -eq 0 ]; then
     echo "Test Passed"
--- viper.c
+++ viper.c
@@ -7,98 +7,71 @@
 #include <linux/netdevice.h>
 #include <linux/version.h>
 
-#define PORT_NAME "viper%d"
+#define PORT_NAME "viperPORT%d"
+#define PC_NAME "viperPC%d"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Elian Chen");
 MODULE_DESCRIPTION("Virtual Ethernet Switch Driver");
 
 /* Customize the number of port in this switch */
-static int num_port = 4;
+static int num_port = 2;
 module_param(num_port, int, 0444);
 MODULE_PARM_DESC(num_port, "Number of port in this virtual switch.");
-
+u32 port_id = 0;
+/* Customize the number of PC */
+static int num_pc = 2;
+module_param(num_pc, int, 0444);
+MODULE_PARM_DESC(num_pc, "Number of PC.");
 
 /* Global structure to maintain all interface */
-struct viper_content{
+struct viper_content {
     /* Point to the list which contain all port interface */
     struct list_head port_list;
+    /* Point to the list which contain all PC interface */
+    struct list_head pc_list;
 };
 /* Port interface pointed to by netdev_priv() */
-struct port_if{
+struct port_if {
     /* Point to the net device */
     struct net_device *ndev;
     struct net_device_stats stats;
+    /* ID */
+    u32 port_id;
     /* Link from viper_content */
     struct list_head port_list;
 };
-static struct viper_content *viper = NULL; 
+/* PC interface pointed to by netdev_priv() */
+struct pc_if {
+    /* Point to the net device */
+    struct net_device *ndev;
+    struct net_device_stats stats;
+    /* Port ID */
+    u32 port_id;
+    /* Link from viper_content */
+    struct list_head pc_list;
+};
+
+static struct viper_content *viper = NULL;
 
 /* Retrieve port interface from net device */
-static inline struct port_if* ndev_get_viper_pif(struct net_device *ndev)
+static inline struct port_if *ndev_get_viper_pif(struct net_device *ndev)
 {
     /* Port interface pointed to by netdev_priv() */
     return (struct port_if *) netdev_priv(ndev);
 }
-
-// Forwarding Database (FDB) to simulate MAC address learning
-#define FDB_SIZE 256
-static struct {
-    unsigned char mac[ETH_ALEN];
-    struct net_device *dev;
-    u16 vlan_id;  // VLAN ID associated with the entry
-} fdb[FDB_SIZE];
-
-static int fdb_lookup(const unsigned char *mac, u16 vlan_id)
+/* Retrieve PC interface from net device */
+static inline struct pc_if *ndev_get_viper_pcif(struct net_device *ndev)
 {
-    for (int i = 0; i < FDB_SIZE; i++) {
-        if (fdb[i].dev && memcmp(fdb[i].mac, mac, ETH_ALEN) == 0 &&
-            fdb[i].vlan_id == vlan_id) {
-            return i;
-        }
-    }
-    return -1;
+    /* Port interface pointed to by netdev_priv() */
+    return (struct pc_if *) netdev_priv(ndev);
 }
 
-static void fdb_add(const unsigned char *mac,
-                    struct net_device *dev,
-                    u16 vlan_id)
-{
-    int i;
-    for (i = 0; i < FDB_SIZE; i++) {
-        if (fdb[i].dev == NULL) {
-            memcpy(fdb[i].mac, mac, ETH_ALEN);
-            fdb[i].dev = dev;
-            fdb[i].vlan_id = vlan_id;
-            return;
-        }
-    }
-}
 
 // The packet reception function (switching logic with VLAN support)
 static int viper_rx(struct sk_buff *skb, struct net_device *dev)
 {
-    unsigned char *mac_dst = eth_hdr(skb)->h_dest;
-    struct net_device *out_dev = NULL;
-    u16 vlan_id = 0;
-
-    // Check if the packet has a VLAN tag and extract the VLAN ID
-    if (skb_vlan_tag_present(skb)) {
-        vlan_id = skb_vlan_tag_get(skb);
-    }
-
-    // Look up the MAC address in the forwarding database (FDB) with VLAN ID
-    int index = fdb_lookup(mac_dst, vlan_id);
-    if (index >= 0) {
-        out_dev = fdb[index].dev;
-    }
-
-    /*if (!out_dev) {
-        pr_warn("viper: MAC address not found in FDB for VLAN %u, dropping packet.\n", vlan_id); dev_kfree_skb(skb); return NET_RX_DROP;
-    }*/
-
-    // Forward the packet to the correct device
-    // skb->dev = out_dev;
+    /* FIXME: Forward the packet to the correct device */
     skb->dev = dev;
     skb->protocol = eth_type_trans(skb, dev);
     skb->ip_summed = CHECKSUM_UNNECESSARY; /* don't check it */
@@ -107,23 +80,34 @@ static int viper_rx(struct sk_buff *skb, struct net_device *dev)
 #else
     return netif_rx(skb);
 #endif
+    /* FIXME: free skb (kfree_skb(skb);)*/
 }
 
 // The packet transmission function (transmitting logic with VLAN support)
 static netdev_tx_t viper_start_xmit(struct sk_buff *skb, struct net_device *dev)
 {
-    unsigned char *mac_src = eth_hdr(skb)->h_source;
-    u16 vlan_id = 0;
-    pr_info("viper: src| %pM\n", mac_src);
-    // Check if the packet has a VLAN tag and extract the VLAN ID
-    if (skb_vlan_tag_present(skb)) {
-        vlan_id = skb_vlan_tag_get(skb);
-    }
+    unsigned char *src_addr = eth_hdr(skb)->h_source;
+    unsigned char *dst_addr = eth_hdr(skb)->h_dest;
+    pr_info("viper: Transmission from %pM to %pM\n", dev->dev_addr, dst_addr);
 
-    // Learn the source MAC address and VLAN ID
-    fdb_add(mac_src, dev, vlan_id);
-    
-    //viper_rx(skb, skb->dev);
+    struct port_if *dest = NULL;
+    /*
+        if (is_broadcast_ether_addr(dst_addr)) {
+            list_for_each_entry (dest, &viper->port_list, port_list) {
+                if (!ether_addr_equal(dest->ndev->dev_addr, src_addr)) {
+                    viper_rx(skb, dest->ndev);
+                }
+            }
+        } else {
+            list_for_each_entry (dest, &viper->port_list, port_list) {
+                if (ether_addr_equal(dest->ndev->dev_addr, dst_addr)) {
+                    viper_rx(skb, dest->ndev);
+                }
+            }
+        }
+    */
+
+    // viper_rx(skb, skb->dev);
     return 0;
     // return dev_queue_xmit(skb);
 }
@@ -151,15 +135,51 @@ static const struct net_device_ops viper_netdev_ops = {
     .ndo_open = viper_eth_open,
     .ndo_stop = viper_eth_stop,
     /* For simplicity, not implementing stats gathering here */
-    .ndo_get_stats = NULL,  
+    .ndo_get_stats = NULL,
 };
 
-static int viper_add_port(int idx){
+/* Construct the port interface */
+static int viper_add_port(int idx)
+{
     struct net_device *ndev = NULL;
     /* Allocate net device context */
     ndev = alloc_netdev(sizeof(struct port_if), PORT_NAME, NET_NAME_ENUM,
                         ether_setup);
-    if (!ndev){
+    if (!ndev) {
+        pr_err("viper: Couldn't allocate space for nedv");
+        return -ENOMEM;
+    }
+    /* Assign viper operations */
+    ndev->netdev_ops = &viper_netdev_ops;
+    /* Close the ARP functionality */
+    ndev->flags |= IFF_NOARP;
+    /* FIXME: Support VLAN */
+
+
+    /* Register net device with the kernel */
+    if (register_netdev(ndev)) {
+        pr_err("viper: Failed to register net device\n");
+        free_netdev(ndev);
+        return -ENODEV;
+    }
+
+    /* Fulfill the port interface */
+    struct port_if *pif = ndev_get_viper_pif(ndev);
+    pif->ndev = ndev;
+    pif->port_id = port_id++;
+    list_add_tail(&pif->port_list, &viper->port_list);
+    pr_info("viper: New PORT %d\n", pif->port_id);
+    return 0;
+}
+
+/* Construct the PC interface */
+static int viper_add_pc(int idx)
+{
+    struct net_device *ndev = NULL;
+    /* Allocate net device context */
+    ndev =
+        alloc_netdev(sizeof(struct pc_if), PC_NAME, NET_NAME_ENUM, ether_setup);
+    if (!ndev) {
         pr_err("viper: Couldn't allocate space for nedv");
         return -ENOMEM;
     }
@@ -172,11 +192,12 @@ static int viper_add_port(int idx){
      */
     char mac[ETH_ALEN];
     /* The first 3~24 bits are vendor identifier */
-    mac[0] = 0x02; /* Locally administered address, unicast (binary: 00000010) */
-    mac[1] = 0xab; 
+    mac[0] =
+        0x02; /* Locally administered address, unicast (binary: 00000010) */
+    mac[1] = 0xab;
     mac[2] = 0xcd;
     /* Assign uniqueness MAC address by idx */
-    mac[3] = idx & (0xFF << 16); 
+    mac[3] = idx & (0xFF << 16);
     mac[4] = idx & (0xFF << 8);
     mac[5] = idx & 0xFF;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
@@ -184,18 +205,22 @@ static int viper_add_port(int idx){
 #else
     memcpy(ndev->dev_addr, mac, ETH_ALEN);
 #endif
-    
+
 
     /* Register net device with the kernel */
     if (register_netdev(ndev)) {
         pr_err("viper: Failed to register net device\n");
+        free_netdev(ndev);
         return -ENODEV;
     }
 
     /* Fulfill the port interface */
-    struct port_if *pif = ndev_get_viper_pif(ndev);
-    pif->ndev = ndev;
-    list_add_tail(&pif->port_list, &viper->port_list);
+    struct pc_if *pcif = ndev_get_viper_pcif(ndev);
+    pcif->ndev = ndev;
+    if (num_port != 0)
+        pcif->port_id = idx % num_port;
+    list_add_tail(&pcif->pc_list, &viper->pc_list);
+    pr_info("viper: New PC%d which belongs to PORT%d\n", idx, pcif->port_id);
     return 0;
 }
 
@@ -208,10 +233,17 @@ static int __init viper_switch_init(void)
         pr_err("viper: Couldn't allocate space for viper_content\n");
         return -ENOMEM;
     }
+    /* Initialize the port interface */
     INIT_LIST_HEAD(&viper->port_list);
-    for (int i=0;i<num_port;++i){
+    for (int i = 0; i < num_port; ++i) {
         viper_add_port(i);
     }
+    /* Initialize the PC interface */
+    INIT_LIST_HEAD(&viper->pc_list);
+    for (int i = 0; i < num_pc; ++i) {
+        viper_add_pc(i);
+    }
+
 
     pr_info("viper: Virtual Ethernet switch driver loaded\n");
     return 0;
@@ -220,10 +252,15 @@ static int __init viper_switch_init(void)
 /* Exit viper */
 static void __exit viper_switch_exit(void)
 {
-    struct port_if *pif=NULL, *safe=NULL;
-    list_for_each_entry_safe(pif, safe, &viper->port_list, port_list){
+    struct port_if *pif = NULL, *safe1 = NULL;
+    list_for_each_entry_safe (pif, safe1, &viper->port_list, port_list) {
         unregister_netdev(pif->ndev);
         free_netdev(pif->ndev);
+    }
+    struct pc_if *pcif = NULL, *safe2 = NULL;
+    list_for_each_entry_safe (pcif, safe2, &viper->pc_list, pc_list) {
+        unregister_netdev(pcif->ndev);
+        free_netdev(pcif->ndev);
     }
     kfree(viper);
 
@@ -232,4 +269,3 @@ static void __exit viper_switch_exit(void)
 
 module_init(viper_switch_init);
 module_exit(viper_switch_exit);
-
